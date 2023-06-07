2e1514e0f50b7070cdc39bdc3b566174ccd32674
Patricia <wambuipatricia3@gmail.com>
2023-06-07 21:06:45 +0300

    Modified main.py

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List, Optional
-from models import Users, Destinations, session
+from models import Users, Destinations, session,Users_Destinations
 from sqlalchemy.orm import joinedload
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+origins=[
+    'http://localhost:3000'
+]
+
+app.add_middleware  (
+   CORSMiddleware,
+   allow_origins = origins,
+   allow_credentials = True,
+   allow_methods = ["*"],
+   allow_headers = ["*"],
+)
+
 
 
 class UsersSchema(BaseModel):
@@ -18,7 +31,7 @@ class UsersSchema(BaseModel):
 
 
 class UpdateUsersSchema(BaseModel):
-    id: Optional[int]
+   
     name: Optional[str]
     password: Optional[str]
 
@@ -33,8 +46,14 @@ class DestinationsSchema(BaseModel):
     description: str
     location: str
     visit_url: str
-    interested: bool
-    user_id: int
+   
+    class Config:
+        orm_mode = True
+
+class InterestSchema(BaseModel):
+    id:int
+    user_id:int
+    destination_id:int
 
     class Config:
         orm_mode = True
@@ -53,47 +72,121 @@ class UpdateDestinationsSchema(BaseModel):
     class Config:
         orm_mode = True
 
+class UpdateInterestSchema(BaseModel):
+    user_id:Optional [int]
+    destination_id:Optional [int]
 
-@app.get("/")
+    class Config:
+        orm_mode = True
+
+
+# @app.get('/')
 def root():
-    return {"message": "Welcome User!"}
+    return {'message' : 'Welcome User!'}
 
-
-@app.get("/users")
+#Returns all users 
+@app.get('/users')
 def get_all_users() -> List[UsersSchema]:
     users = session.query(Users).all()
     return users
 
-
-@app.get("/destinations")
+@app.get('/destinations')
 def get_all_destinations() -> List[DestinationsSchema]:
-    destinations = (
-        session.query(Destinations).options(joinedload(Destinations.user)).all()
-    )
-    destinations_schema = [
-        DestinationsSchema.from_orm(destination) for destination in destinations
-    ]
-    return destinations_schema
+    destinations = session.query(Destinations).all()
+    return destinations
 
+#returns all interests
+@app.get('/interests')
+def get_all_interests():
+    interests = session.query(Users_Destinations).all()
+    return interests
+
+#create new destination data
+@app.post('/update/destinations')
+def add_destinations(destinations: UpdateDestinationsSchema)->DestinationsSchema:
+    destin = Destinations(**dict(destinations))
+    # session.add(destin)
+    session.add(destin)
+    session.commit()
+
+    return destin 
+
+#create new user data
+@app.post('/update/users')
+def add_users(users: UpdateUsersSchema)->UsersSchema:
+    user0 = Users (**dict(users))
+    session.add(user0)
+    session.commit()
+    
+    return user0 
+
+#create new interest
+@app.post('/update/interests')
+def add_interest(interests: UpdateInterestSchema)->InterestSchema:
+    interest = Users_Destinations(**dict(interests))
+    session.add(interest)
+    session.commit()
+    return interest
+
+#updates user information
+@app.patch('/update/user/{id}')
+def update_user(id:int, payload: UpdateUsersSchema) -> UsersSchema:
+    updated_user = session.query(Users).filter_by(id = id).first()
+    if not updated_user:
+        raise HTTPException(status_code=404, detail=f'The user with id {id} does not exist')
+    for key, value in payload.dict(exclude_unset=True).items():
+        setattr(updated_user, key, value)
+    session.commit()
+    return updated_user
+
+#updates destination information
+@app.patch('/update/destination/{id}')
+def update_destination(id:int, payload: UpdateDestinationsSchema) -> DestinationsSchema:
+    updated_destination = session.query(Destinations).filter_by(id=id).first()
+    if not updated_destination:
+        raise HTTPException(status_code=404, detail=f'The destination with id {id} does not exist')
+    for key, value in payload.dict(exclude_unset=True).items():
+        setattr(updated_destination, key, value)
+    session.commit()
+    return updated_destination
+
+#updates interest information
+@app.patch('/update/interest/{id}')
+def update_interest(id:int, payload: UpdateInterestSchema)->InterestSchema:
+    updated_interest = session.query(Users_Destinations).filter_by(id=id).first()
+    if not updated_interest:
+        raise HTTPException(status_code=404, detail=f'The interest with id {id} does not exist')
+    for key, value in payload.dict(exclude_unset=True).items():
+        setattr(updated_interest, key, value)
+    session.commit()
+    return updated_interest
 
 # delete a user
-
-
-@app.delete("/users/{id}")
+@app.delete("/delete/users/{id}")
 def delete_user(id: int):
     user = session.query(Users).filter_by(id=id).first()
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     session.delete(user)
     session.commit()
-    return {"message": "User deleted successfully"}
+    return {"message": f"User with id:{id} deleted successfully"}
 
-@app.delete("/destinations/{id}")
+#delete a destination
+@app.delete("/delete/destinations/{id}")
 def delete_destination(id: int):
     destination = session.query(Destinations).filter_by(id=id).first()
     if not destination:
         raise HTTPException(status_code=404, detail="Destination not found")
     session.delete(destination)
     session.commit()
-    return {"message": "Destination deleted successfully"}
+    return {"message": f"Destination with id:{id} deleted successfully"}
 
+#delete an interest
+@app.delete('/delete/interest/{id}')
+def delete_interest(id:int):
+    interest = session.query(Users_Destinations).filter_by(id=id).first()
+    if not interest:
+        raise HTTPException(status_code=404, detail='Interest not found')
+    session.delete(interest)
+    session.commit()
+    return {'message':f'Interest with id:{id} deleted successfully'}
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship, backref, sessionmaker, declarative_base
+from sqlalchemy.ext.associationproxy import association_proxy
 
 engine = create_engine('sqlite:///project.db')
 
@@ -12,7 +13,8 @@ class Users(Base):
     name = Column(String())
     password = Column(String(8))
 
-    destinations = relationship('Destinations', backref = backref('user'), cascade = 'all, delete-orphan')
+    destinations = association_proxy('users_destinations', 'destinations', creator=lambda ds: Users_Destinations(destinations=ds))
+    users_destinations = relationship('Users_Destinations',back_populates='user', cascade='all, delete-orphan')
 
 class Destinations(Base):
     __tablename__ = 'destinations'
@@ -20,10 +22,21 @@ class Destinations(Base):
     name = Column(String())
     image = Column(String())
     description = Column(String())
+    category = Column(String())
     location = Column(String())
     visit_url = Column(String())
-    interested = Column(Boolean(False))
+
+    users = association_proxy('users_destinations', 'users', creator=lambda us: Users_Destinations(users=us))
+    users_destinations = relationship('Users_Destinations',back_populates='destination', cascade='all, delete-orphan')
+
+class Users_Destinations(Base):
+    __tablename__='users_destinations'
+    id = Column(Integer(), primary_key=True)
     user_id = Column(Integer(), ForeignKey('users.id'))
+    destination_id = Column(Integer(), ForeignKey('destinations.id'))
+
+    user = relationship('Users', back_populates='users_destinations')
+    destination = relationship('Destinations', back_populates='users_destinations')
 
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind = engine)
