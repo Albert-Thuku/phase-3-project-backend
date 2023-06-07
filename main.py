51e7ea8a2630600cf2ddf3fd3fc29e3191984ef4
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-06-07 09:21:09 +0300

    Modified CORS

--- main.py
+++ main.py
@@ -3,8 +3,21 @@ from pydantic import BaseModel
 from typing import List, Optional
 from models import Users, Destinations, session
 from sqlalchemy.orm import joinedload
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins=[
+    'http://localhost:3000'
+]
+
+app.add_middleware  (
+   CORSMiddleware,
+   allow_origins = origins,
+   allow_credentials = True,
+   allow_methods = ["*"],
+   allow_headers = ["*"],
+)
 
 class UsersSchema(BaseModel):
     id:int
@@ -47,6 +60,8 @@ class UpdateDestinationsSchema(BaseModel):
 
     class Config:
         orm_mode = True
+        
+    
 
 @app.get('/')
 def root():
