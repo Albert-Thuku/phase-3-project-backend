eed9f1cb37a9a708e8bd906e112747c85b34b0bf
Albert Thuku <athuku02@gmail.com>
2023-06-09 19:49:20 +0300

    Changed origin URL

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins=[
-    'http://localhost:3000'
+    'https://travel-guru-backend.onrender.com'
 ]
 
 app.add_middleware  (
